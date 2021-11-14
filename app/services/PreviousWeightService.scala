65520827cfd2710871835544cb1a10d3e4be8230
Christian-QA <credfernbus@gmail.com>
2021-11-14 19:12:21 +0000

    Finished previousWeightList service method

--- app/services/PreviousWeightService.scala
+++ app/services/PreviousWeightService.scala
@@ -4,24 +4,41 @@ import com.google.inject.Inject
 import connectors.PolybodyConnector
 import errors.CustomErrorHandler
 import models.PreviousWeight
+import ujson.Value
 
 import java.time.LocalDate
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.{ExecutionContext, Future}
 
 class PreviousWeightService @Inject()(polybodyConnector: PolybodyConnector)(implicit ec: ExecutionContext) {
 
-  def getPreviousWeights(username: String): Future[Either[CustomErrorHandler, PreviousWeight]] = {
-    val data = polybodyConnector.getPreviousWeights(username)
+  def getPreviousWeights(username: String): Future[Either[CustomErrorHandler, List[PreviousWeight]]] = {
+    val data: Future[Either[CustomErrorHandler, ArrayBuffer[Value]]] = polybodyConnector.getPreviousWeights(username)
 
     data.map {
       case Right(value) =>
-        Right(
-          PreviousWeight(
-            LocalDate.parse(value(0)(0)("dateTime").str),
-            value(0)(0)("weight").num
-          )
-        )
+        val parsedInput: ArrayBuffer[Value] = value(0).arr
+
+        @tailrec
+        def previousWeightList(input: List[PreviousWeight], acc: Int): List[PreviousWeight] = {
+
+          if (acc < parsedInput.length) {
+            val weight = PreviousWeight(
+              LocalDate.parse(parsedInput(acc)("dateTime").str),
+              parsedInput(acc)("weight").num
+            ) :: input
+            previousWeightList(weight, acc + 1)
+          } else {
+            input
+          }
+        }
+        Right(previousWeightList(List.empty, 0))
       case Left(value) => Left(value)
     }
+
+
+
+
   }
 }
