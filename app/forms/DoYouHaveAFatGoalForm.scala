a23e7498fc54dbd84e2b0c62acb6af2ecfdca4a0
Christian-QA <credfernbus@gmail.com>
2022-03-22 23:02:33 +0000

    Added messages

--- app/forms/DoYouHaveAFatGoalForm.scala
+++ app/forms/DoYouHaveAFatGoalForm.scala
@@ -15,7 +15,7 @@ object DoYouHaveAFatGoalForm {
       mapping(
         "doYouHaveAFatGoal" -> number
         //          .verifying(
-        //            error = "Please enter your Page2Age using the Slider or the text box",
+        //            error = "Please enter your Age using the Slider or the text box",
         //            constraint = e => e < 1 && e > 130
         //          )
       )(DoYouHaveAFatGoalForm.apply)(DoYouHaveAFatGoalForm.unapply)
