abeb45d1dc46383183f9eb5672349a5aa665efd6
Christian-QA <credfernbus@gmail.com>
2022-07-20 21:28:12 +0100

    Reverted WireMock, will do on different ticket

--- test/connectors/PolybodyConnectorSpec.scala
+++ test/connectors/PolybodyConnectorSpec.scala
@@ -1,7 +1,5 @@
 package connectors
 
-import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, urlEqualTo}
 import errors.{
   CustomErrorHandler,
   CustomNoContentResponse,
@@ -10,10 +8,10 @@ import errors.{
 import org.mockito.Mockito.when
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import play.api.Application
-import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
+import play.api.http.Status.INTERNAL_SERVER_ERROR
 import play.api.inject.guice.GuiceApplicationBuilder
 import ujson.Value
-import utils.{BaseSpec, WireMockHelper}
+import utils.BaseSpec
 import utils.UserDetails.{passUserUjson, passUsername}
 
 import scala.collection.mutable.ArrayBuffer
@@ -23,8 +21,7 @@ import scala.concurrent.{Await, Future}
 class PolybodyConnectorSpec
     extends BaseSpec
     with ScalaFutures
-    with IntegrationPatience
-    with WireMockHelper {
+    with IntegrationPatience {
 
   lazy val polybodyConnector: PolybodyConnector = mock[PolybodyConnector]
 
@@ -33,38 +30,47 @@ class PolybodyConnectorSpec
 
   "PolybodyConnector" when {
     "getUserDetails is called" must {
-      val url = s"/findSpecificUser/$passUsername"
-
       "return a Future[ArrayBuffer[Value]] containing user data if the user exists" in {
 
-        server.stubFor(
-          WireMock
-            .get(urlEqualTo(url))
-            .willReturn(
-              aResponse()
-                .withStatus(OK)
-                .withBody(passUserUjson.toString)
-            )
+        when(polybodyConnector.getUserDetails(passUsername))
+          .thenReturn(Future.successful(Right(passUserUjson)))
+
+        val response: Future[Either[CustomErrorHandler, ArrayBuffer[Value]]] =
+          polybodyConnector.getUserDetails(passUsername)
+
+        val result: Either[CustomErrorHandler, ArrayBuffer[Value]] =
+          Await.result(response, Duration(5, "seconds"))
+
+        result mustBe Right(passUserUjson)
+      }
+      "return a UserNoContentResponse if the user doesn't exist" in {
+
+        when(polybodyConnector.getUserDetails(passUsername))
+          .thenReturn(Future.successful(Left(CustomNoContentResponse)))
+
+        val response: Future[Either[CustomErrorHandler, ArrayBuffer[Value]]] =
+          polybodyConnector.getUserDetails(passUsername)
+
+        val result: Either[CustomErrorHandler, ArrayBuffer[Value]] =
+          Await.result(response, Duration(5, "seconds"))
+
+        result mustBe Left(CustomNoContentResponse)
+      }
+      "return a UserUpstreamResponse if the connection to the backend fails" in {
+
+        when(polybodyConnector.getUserDetails(passUsername)).thenReturn(
+          Future
+            .successful(Left(CustomUpstreamResponse("", INTERNAL_SERVER_ERROR)))
         )
 
-//        when(polybodyConnector.getUserDetails(passUsername))
-//          .thenReturn(Future.successful(Right(passUserUjson)))
+        val response: Future[Either[CustomErrorHandler, ArrayBuffer[Value]]] =
+          polybodyConnector.getUserDetails(passUsername)
 
-        val response = polybodyConnector.getUserDetails(passUsername)
+        val result: Either[CustomErrorHandler, ArrayBuffer[Value]] =
+          Await.result(response, Duration(5, "seconds"))
 
-        response.futureValue mustBe Right(passUserUjson)
+        result mustBe Left(CustomUpstreamResponse("", INTERNAL_SERVER_ERROR))
       }
-
-//      "return a UserNoContentResponse if the user doesn't exist" in {
-//
-//
-//        result mustBe Left(CustomNoContentResponse)
-//      }
-//      "return a UserUpstreamResponse if the connection to the backend fails" in {
-//
-//
-//        result mustBe Left(CustomUpstreamResponse("", INTERNAL_SERVER_ERROR))
-//      }
     }
   }
 }
